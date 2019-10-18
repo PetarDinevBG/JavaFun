2d44ea620b8845025143e23848aa87c08e4e2228
PetarShadow <pdinev@u.rochester.edu>
2019-10-18 05:00:31 -0500

    Added some skeleton methods for the AI

--- src/ComputerPlayer.java
+++ src/ComputerPlayer.java
@@ -1,12 +1,19 @@
 
 public class ComputerPlayer implements Player {
-
+	FullBoard thinkBoard;
 	@Override
 	public Move think() {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
+	
+	public void setThinkBoard(FullBoard thinkBoard){
+		this.thinkBoard = thinkBoard;
+	}
+	
+	public double heuristicFunc(FullBoard board){
+		return 0;
+	}
 
 
 }
